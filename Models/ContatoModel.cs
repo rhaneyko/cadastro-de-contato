323227fc707e7594e2bdebb57a0ca53f04a0d685
rhaneyko <rhaneykohonrio@gmail.com>
2022-09-14 16:51:44 -0400

    C# App

--- Models/ContatoModel.cs
+++ Models/ContatoModel.cs
@@ -3,7 +3,7 @@
     public class ContatoModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Nome { get; set; }
         public string Email { get; set; }
         public string Celular { get; set; }
 
